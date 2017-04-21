715a8eb443282b06f60eaf3c3bba0603bcf4b501
twt313@gmail.com <twt313@gmail.com>
2017-04-21 14:59:29 +0800

    'add'

---
+++ index.js
@@ -0,0 +1,36 @@
+var request = require('request');  
+var http = require('http');  
+var qs = require('querystring');  
+  
+var opt = {  
+    host:'127.0.0.1',  
+    port:'8888',  
+    method:'POST',  
+    path:'/',  
+    headers:{  
+        'content-type': "application/json"  
+        //'accept-encoding': 'gzip'  
+    }  
+};  
+  
+var body = '';  
+var req = http.request(opt, function(res) {  
+  
+    console.log("Got response: " + res.statusCode);  
+    res.on('data',function(d){  
+        body += d;  
+    });  
+    res.on('end', function(){  
+        console.log(res.headers);  
+        console.log(body)  
+    });  
+});  
+  
+req.on('error', function(e) {  
+    console.log("Got error: " + e.message);  
+});  
+  
+var data = {num:"1",num1:"123",type:"add"};  
+  
+req.write(qs.stringify(data));  
+req.end()
